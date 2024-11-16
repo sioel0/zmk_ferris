08e2e9c832089e3aa40322cc290b493e7ac4e2d6
sioel0 <sioel0@pm.me>
2024-11-16 20:32:58 +0100

    Added key definition file and updated keymap

---
+++ config/keys.h
@@ -0,0 +1,31 @@
+// this fine contains all the definitions for the keyboard shortcuts mapped as single keys
+
+// powertoys specific keys
+#define SNIP LG(T)
+#define SSNIP LG(LS(R))
+#define HCLCK LG(LS(H))
+#define MOU_AIM LG(LA(P))
+#define TEXTR LG(LS(T))
+#define PT_RUN LA(SPC)
+
+// general shortcuts
+#define COPY LC(C)
+#define PASTE LC(V)
+#define CUT LC(X)
+#define NWIN LA(TAB)
+#define PWIN LA(LS(TAB))
+#define NTAB LC(TAB)
+#define PTAB LC(LS(TAB))
+#define LOCK LG(L)
+#define SAVE LC(S)
+#define UNDO LC(Z)
+#define FEXPL LG(E)
+
+// linux terminal
+#define TMUX_LEAD LC(A)
+#define KILL_LINE LC(K)
+#define CLEAR LC(L)
+#define LOGOUT LC(D)
+
+// rename key to have better understanding
+#define STAMP PSCRN

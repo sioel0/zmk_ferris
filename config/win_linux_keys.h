4f40eaca1ad503dcd2d7051459ed9956d80fb2b2
sioel0 <sioel0@pm.me>
2025-12-26 20:56:40 +0100

    Updated win/linux mappings macro

--- config/win_linux_keys.h
+++ config/win_linux_keys.h
@@ -1,11 +1,11 @@
 // this file contains all the Windows/Linux shortcuts needed
 
-#define CP LC(C) // copy
-#define PASTE LC(V) // paste
-#define LOCK LG(L) // lock screen
-#define SAVE LC(S) // save
-#define RAYCAST LA(SPC) // raycast
-#define UNDO LC(Z) // undo
-#define CUT LC(X) // undo
-#define EXPL LG(E) // exploarer
-#define S_ALL LC(A) // select all
+#define W_CP LC(C) // copy
+#define W_PASTE LC(V) // paste
+#define W_LOCK LG(L) // lock screen
+#define W_SAVE LC(S) // save
+#define W_RAYCAST LA(SPC) // raycast
+#define W_UNDO LC(Z) // undo
+#define W_CUT LC(X) // undo
+#define W_EXPL LG(E) // exploarer
+#define W_S_ALL LC(A) // select all

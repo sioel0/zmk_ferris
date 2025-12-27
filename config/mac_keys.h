0a9d7561e00fbbabba5efb66a65308ad1a062c89
sioel0 <sioel0@pm.me>
2025-12-27 11:33:51 +0100

    Swapped BLT and MBLT layers, added Hyper key on Mac, refactored Mac_keys and Win_keys files

--- config/mac_keys.h
+++ config/mac_keys.h
@@ -1,11 +1,13 @@
-// this file contains all the Mac Os shortcuts that I need
-
-#define M_CP LG(C) // copy
-#define M_PASTE LG(V) // past
-#define M_LOCK LG(LC(Q)) // lock screen
-#define M_SAVE  LG(S) // save
-#define M_RAYCAST LG(SPC) // raycast
-#define M_UNDO LG(Z) // undo
-#define M_CUT LG(X) // cut
-#define FINDER LG(E) // finder
-#define M_S_ALL LG(A) // select all
+/**
+ * Definition of all the Mac Os shortcuts in a more readable way
+ */
+#define M_CP       LG(C)              // copy
+#define M_PASTE    LG(V)              // paste
+#define M_LOCK     LG(LC(Q))          // lock screen
+#define M_SAVE     LG(S)              // save
+#define M_RAYCAST  LG(SPC)            // raycast
+#define M_UNDO     LG(Z)              // undo
+#define M_CUT      LG(X)              // cut
+#define FINDER     LG(E)              // finder
+#define M_S_ALL    LG(A)              // select all
+#define HYPER      LG(LC(LA(LSHIFT))) // press all 4 modifiers -> raycast hyper key
--- config/win_linux_keys.h
+++ config/win_linux_keys.h
@@ -1,11 +1,13 @@
-// this file contains all the Windows/Linux shortcuts needed
+/**
+ * Definition of all the Windows/Linux keyboard shortcuts in a more readable way
+ */
 
-#define W_CP LC(C) // copy
-#define W_PASTE LC(V) // paste
-#define W_LOCK LG(L) // lock screen
-#define W_SAVE LC(S) // save
-#define W_RAYCAST LA(SPC) // raycast
-#define W_UNDO LC(Z) // undo
-#define W_CUT LC(X) // undo
-#define W_EXPL LG(E) // exploarer
-#define W_S_ALL LC(A) // select all
+#define W_CP       LC(C)   // copy
+#define W_PASTE    LC(V)   // paste
+#define W_LOCK     LG(L)   // lock screen
+#define W_SAVE     LC(S)   // save
+#define W_RAYCAST  LA(SPC) // raycast
+#define W_UNDO     LC(Z)   // undo
+#define W_CUT      LC(X)   // undo
+#define W_EXPL     LG(E)   // exploarer
+#define W_S_ALL    LC(A)   // select all
